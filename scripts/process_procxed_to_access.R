2beb6fe89e49b8f3986e455906b1ac33dee4b642
graemegowans <graemegowans85@gmail.com>
2020-07-13 11:29:29 +0100

    Update for new site.

--- scripts/process_procxed_to_access.R
+++ scripts/process_procxed_to_access.R
@@ -3,14 +3,14 @@
 # Graeme Gowans
 # October 2019
 # Latest update author - Graeme
-# Latest update date - March 2020
-# reflect simpler process for beta website
-# Extraction
+# Latest update date - July 2020
+# updates for new website
+
 # R Studio Desktop
-# 3.5.1
+# 3.6.1
 # Description of content
 # processes output from ProcXed monthly report to format 
-# required for Access to update ISD Forthcoming page
+# required for forthcoming page on PHS site
 # Approximate run time < 5 seconds
 ##########################################################
 
@@ -26,31 +26,28 @@ library(janitor)
 library(readxl)
 library(lubridate)
 library(tidylog)
-library(fuzzyjoin)
 library(glue)
 
 # use date in filename
-filename <- "Feb-20"
+filename <- "Aug_20"
 
 #use one of these to define path
-location <- file.path("F:", "PHI", "Publications", "Governance", "Pre Announcement", "Outputs", "2020")
-location <- "F:/PHI/Publications/Governance/Pre Announcement/Outputs/2020/"
+#location <- file.path("F:", "PHI", "Publications", "Governance", "Pre Announcement", "Outputs", "2020", "2020-05-May")
+location <- "F:/PHI/Publications/Governance/Pre Announcement/Outputs/2020/2020-08-Aug"
 
 # Set limit for showing full date
-date_limit <- "2020-04-01"
-
+date_limit <- "2020-10-01"
 
 ### 2 check new and changed dates ----
 
 #this comes from a different ProcXed report
 #load publications with changed dates
-#clean col names, make new columns to parse dates for checking
-date_changed <- read_xlsx(glue("{location}/ISD Dates Changed ",
+date_changed <- read_xlsx(glue("{location}/ISD_Dates_Changed_",
                                "{filename}.xlsx"), col_types = "text") %>% 
                 clean_names() %>% 
-                select(-producer_organisation, -statistics_type, -url_address) %>%   
+                select(-producer_organisation, -statistics_type, -url_address) %>% 
                 mutate(prev = if_else(is.na(ymd(previous_publication_date)), 
-                                      dmy(paste0("01-", previous_publication_date)),
+                                      dmy(paste0("01-",previous_publication_date)),
                                       ymd(previous_publication_date))) %>% 
                 mutate(curr = if_else(is.na(ymd(current_publication_date)), 
                                       dmy(paste0("01-", current_publication_date)),
@@ -72,12 +69,18 @@ date_changed %>% filter(previous_publication_date == "NEWLY ADDED" &
 
 # Read the data - these are files from ProcXed report
 # Rename column names for Access database
-new_pubs <- read_xlsx(glue("{location}/ISD Forthcoming Publications ",
+#these are still called ISD for now - need separate report
+new_pubs <- read_xlsx(glue("{location}/ISD_Forthcoming_Publications_",
                            "{filename}.xlsx"), col_types = "text") %>%
-            rename(DatePublished = `Publication Date`, 
-                   Title = `Publication Series`, 
-                   Synopsis = `Synopsis`) %>% 
-            select(DatePublished, Title, Synopsis)
+                      rename(DatePublished = `Publication Date`, 
+                            Title = `Publication Series`, 
+                            Synopsis = `Synopsis`, 
+                            ContactName = `Contact Details`) %>% 
+                      select(DatePublished, Title, Synopsis)
+
+#check for encoding problems
+new_pubs %>% mutate(encd = Encoding(Synopsis)) %>% filter(encd != "unknown")
+new_pubs %>% mutate(encd = Encoding(Title)) %>% filter(encd != "unknown")
 
 ### 4 Formatting Dates ----
 
@@ -86,105 +89,92 @@ new_pubs <- read_xlsx(glue("{location}/ISD Forthcoming Publications ",
 # start of the date and parse again, otherwise just parse
 new_pubs %<>% 
   mutate(DatePublished = if_else(is.na(dmy(DatePublished)), 
-                                    dmy(paste0("01-", DatePublished)),
-                                    dmy(DatePublished)))
+                                 dmy(paste0("01-", DatePublished)),
+                                 dmy(DatePublished)))
 
 # If DatePublished is before date_limit then show full date
 # If not then show DatePublished as the first of the month
 new_pubs %<>% 
   mutate(DatePublished = if_else(DatePublished < date_limit, 
-                                    DatePublished, 
-                                    floor_date(DatePublished, unit = "month")))
+                                 DatePublished, 
+                                 floor_date(DatePublished, unit = "month")))
 
-# might not be needed anymore?
 # If publication date is after date_limit then add 1 to NotSet
 # If publication date is before date_limit then leave blank
 new_pubs %<>% 
   mutate(NotSet = if_else(DatePublished < date_limit, "", "1"))
 
 #check all dates are Tuesday, show those that aren't
-#will need changed once HPS are included
 new_pubs %>% 
   mutate(day_of_week = wday(DatePublished, label = TRUE)) %>% 
   filter(DatePublished < date_limit & day_of_week != "Tue")
 
-######### remember to rerun script to add new topics ##############
+
+### 5 Formatting titles ----
+
+#replace known problems
+#HPV vaccination is only one
+new_pubs <- new_pubs %>% 
+              mutate(Title = str_replace(Title,
+                     pattern = "HPV Vaccination Statistics for Men who have Sex with Men, Scotland, HPV in MSM - October 2020",
+                     replacement = "HPV Vaccination Statistics for Men who have Sex with Men, Scotland"))
 
 # Process the title to get rid of the edition (e.g. October 2020 release)
 # If only 1 comma, then take text before comma
 # If more than 1 comma, then flag title as needing manual clean up
+new_pubs <- new_pubs %>% 
+              mutate(comma_count = str_count(Title, ",")) %>%
+              separate(Title, sep = ",", into = c("series", "edition"), remove = FALSE) %>%
+              mutate(Title = if_else(comma_count == 1, series, Title),
+                      title_flag = if_else(comma_count > 1, "TRUE", "")) %>% 
+              select(-c(comma_count, series, edition))
 
-new_pubs %<>% 
-      mutate(comma_count = str_count(Title, ",")) %>%
-      separate(Title, sep = ",", into = c("series", "edition"), remove = FALSE) %>%
-      mutate(Title = if_else(comma_count == 1, series, Title),
-              title_flag = if_else(comma_count > 1, "TRUE", "")) %>% 
-      select(-c(comma_count, series, edition))
+#any problems? if only above can delete
+new_pubs %>% filter(title_flag == "TRUE")
 
-### 7 General Formatting ----
+if(nrow(filter(new_pubs, title_flag == "TRUE")) == 0) {
+new_pubs <- select(new_pubs, -title_flag)
+}
 
-# Add rescheduled_to and revised columns as NA
-# these will need to be manually adjusted
-# Add html tags for synopsis on website
-# Change date format to dd/mm/yy
-# Rearrange columns
-
-new_pubs %<>% 
-  mutate(RescheduledTo = NA, 
-         Revised = NA, 
-         Synopsis = glue("<p>{Synopsis}</p>"), 
-         DatePublished = format(DatePublished, "%d/%m/%y")) %>% 
-  select(title_flag, DatePublished, Title, Synopsis, 
-         RescheduledTo, Revised, NotSet)
-
-
-### 8 Add Rescheduled Publications ----
-
-# Remember to add reason for delay in Synopsis
-# There should be an entry already for the rescheduled date
-# ProcXed can run reports to show publications where dates 
-# have changed to highlight these
-
-rescheduled_pubs <- tibble(
-          DatePublished = "DD/MM/YYYY",
-          Title = "title_here",
-          HealthTopic = "topic_here",
-          Synopsis = "<p>synopsis_here</p>",
-          ContactName = paste("analyst_1 tel. 123 456", 
-                               "analyst_2 tel. 123 456"),
-          RescheduledTo = "DD/MM/YYYY",
-          Revised = NA, 
-          NotSet = NA)
-
-# Bind resched pubs
-new_pubs <- bind_rows(new_pubs, rescheduled_pubs)
-
-
-### 9 Remove Duplicate ED Weekly Publications ----
+### 6 Remove Duplicate ED Weekly Publications ----
 
 # Make a key of DatePublished, Title and Synopsis
 # Keep only the distinct ones
 # Should be only weekly ED ones from later months
 
-# This shows what will be removed, can check only ED weekly
-dups <- new_pubs %>%
-        mutate(dupkey = paste0(DatePublished, Title, Synopsis)) %>% 
-        mutate(is_dup = duplicated(dupkey)) %>%
-        filter(is_dup == TRUE)
+#generate key
+new_pubs <- new_pubs %>%
+              mutate(dupkey = glue("{DatePublished}_{Title}_{Synopsis}"))
+
+#show duplicates
+new_pubs %>% filter(duplicated(dupkey))
 
 # Remove these rows
 new_pubs %<>%
-  mutate(dupkey = paste0(DatePublished,Title, Synopsis)) %>%
   distinct(dupkey, .keep_all = TRUE) %>%
   select(-dupkey)
 
-### 10 Save as .csv ----
+### 7 general formatting ----
+
+#check formatting - should all be "unknown" so this should return any issues
+#if so, go change in excel or procxed
+new_pubs %>% mutate(encd = Encoding(Synopsis)) %>% filter(encd != "unknown")
+new_pubs %>% mutate(encd = Encoding(Title)) %>% filter(encd != "unknown")  
+
+#add tag to synopsis and change date
+new_pubs <- new_pubs %>% 
+  mutate(Synopsis = glue("<p>{Synopsis}</p>"), 
+         DatePublished = format(DatePublished, "%d/%m/%y"))
+
+### 8 Save as .csv ----
 
 # Export file as .csv
 # Use write.csv as write_csv sometimes gives weird encoding in access
 
-write.csv(new_pubs, 
-          glue("{location}/ISD_{filename}_processed_xl.csv"), 
+date_to_use <- format(today(), "%Y_%m_%d")
+
+write.csv(new_pubs,
+          file = glue("{location}/ISD_{filename}_processed_beta_{date_to_use}.csv"), 
           na = "",
           row.names = FALSE)
 
